7e76c2aaa001cbdf1ddea2e7e8e646e5a6111c08
Paul Onions <paul.onions@acm.org>
2015-07-28 22:18:25 +0100

    Added output-command option to axiom-process-redirect-send-command.

--- ob-axiom.el
+++ ob-axiom.el
@@ -85,8 +85,7 @@ This function is called by `org-babel-execute-src-block'."
       (with-axiom-process-query-buffer
        (dolist (line lines)
          (beginning-of-line)
-         (insert line)
-         (axiom-process-redirect-send-command line (current-buffer) nil t t))
+         (axiom-process-redirect-send-command line (current-buffer) nil t t t))
        (buffer-substring (point-min) (point-max))))))
 
 ;;; Internal helper functions
